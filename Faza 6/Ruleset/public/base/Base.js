214eb306583237ffe678dcca7afababc9eda872c
DamjanTM <39652602+DamjanTM@users.noreply.github.com>
2020-05-28 15:19:15 +0200

    Reorganizovan public folder

--- Faza 6/Ruleset/public/base/Base.js
+++ Faza 6/Ruleset/public/base/Base.js
@@ -12,10 +12,10 @@ function setDarkTheme() {
     root.style.setProperty('--shadow-brand-color', "#f7bb61c7");
     root.style.setProperty('--hover-brand-color', "#f7B37D");
     root.style.setProperty('--navbar-color', "transparent");
-    root.style.setProperty('--content-color', "#23222399");
+    root.style.setProperty('--table-color', "#23222399");
     root.style.setProperty('--text-brand-color', "black");
     root.style.setProperty('--text-navbar-color', "white");
-    root.style.setProperty('--text-content-color', "#F69D52");
+    root.style.setProperty('--text-table-color', "#F69D52");
     root.style.setProperty('--input-bg-navbar-color', "#ffffff66");
     root.style.setProperty('--input-bg-brand-color', "#ffffff66");
     root.style.setProperty('--main-success-color', "#7faa52");
@@ -37,15 +37,25 @@ function setLightTheme() {
     root.style.setProperty('--shadow-brand-color', "#f7bb61c7");
     root.style.setProperty('--hover-brand-color', "#f7B37D");
     root.style.setProperty('--navbar-color', "#232223");
-    root.style.setProperty('--content-color', "#232223");
+    root.style.setProperty('--table-color', "#232223");
     root.style.setProperty('--text-brand-color', "black");
-    root.style.setProperty('--text-content-color', "#F3F6F5");
+    root.style.setProperty('--text-table-color', "#F3F6F5");
     root.style.setProperty('--input-bg-navbar-color', "#ffffff66");
     root.style.setProperty('--input-bg-brand-color', "#ffffff66");
     root.style.setProperty('--main-success-color', "#7faa52");
     root.style.setProperty('--border-success-color', "#6e9445");
     root.style.setProperty('--hover-success-color', "#9cb682");
     root.style.setProperty('--shadow-success-color', "#93be65c7");
+
+    root.style.setProperty('--default-text-color', "#232223");
+    root.style.setProperty('--input-text-color', "#232223");
+    root.style.setProperty('--input-bg-color', "white");
+
+    root.style.setProperty('--btn-bg', "#F69D52");
+    root.style.setProperty('--btn-bg-hover', "#f0ad77");
+    root.style.setProperty('--btn-text', "white");
+    root.style.setProperty('--btn-text-hover', "white");
+    root.style.setProperty('--btn-border-hover', "#F69D52");
 
     $(".table-light").each(function () {
         $(this).addClass("table-dark");
--- Faza 6/Ruleset/public/base/Navbar.php
+++ Faza 6/Ruleset/public/base/Navbar.php
@@ -4,7 +4,7 @@
     <title>Ruleset by team DOGS</title>
     <!-- Version: 1.0 -->
     <meta charset="UTF-8">
-    <link rel ="shortcut icon" type="image/png" href="assets/navbar/corgi_pixel.png">
+    <link rel="shortcut icon" type="image/png" href="assets/navbar/corgi_pixel.png">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
         integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous">
@@ -16,15 +16,17 @@
         integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous">
     </script>
 
+    <link rel="stylesheet" href="Base.css" />
     <link rel="stylesheet" href="Navbar.css" />
 </head>
 
 <body>
     <div class="header">
-        <nav class="navbar bg-dark navbar-dark"> <!-- navbar-expand-lg -->
-            <a class="navbar-brand" href="https://www.youtube.com/watch?v=EWMPVn1kgIQ">
-                <img id='logoRuleImage' src="assets/navbar/rule_icon.png" alt="Logo" class='logoImage'>
-                <img id='logoSetImage' src="assets/navbar/set_icon.png" alt="Logo" class='logoImage'>
+        <nav class="navbar bg-dark navbar-dark">
+            <!-- navbar-expand-lg -->
+            <a class="navbar-brand" href="../main/main.html">
+                <img id='logoRuleImage' src="../assets/navbar/rule_icon.png" alt="Logo" class='logoImage'>
+                <img id='logoSetImage' src="../assets/navbar/set_icon.png" alt="Logo" class='logoImage'>
             </a>
 
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarElements"
@@ -34,20 +36,17 @@
 
             <div class="collapse navbar-collapse" id="navbarElements">
                 <ul class="navbar-nav ml-auto">
-                    <li class="nav-item active">
-                        <a class="nav-link" href="https://www.crazygames.com/game/uno-online">Play</a>
+                    <li class="nav-item">
+                        <a class="nav-link" href="../decklab/decklab.html">Make Ruleset</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="login.html">Login</a>
+                        <a class="nav-link" href="../deck_list/deckList.html">Find Ruleset</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="register.html">Register</a>
+                        <a class="nav-link" href="../login/login.html">Login</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="decklab.html">Make Ruleset</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="deckList.html">Find Ruleset</a>
+                        <a class="nav-link" href="../register/register.html">Register</a>
                     </li>
                 </ul>
             </div>
